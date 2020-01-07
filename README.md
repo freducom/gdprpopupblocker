2e7b7d248b744acdaa39937db10270b65528b2c1
Fredrik Rönnlund <fredu@fredu.com>
2020-01-07 22:13:47 +0200

    First version of product

---
+++ README.md
@@ -0,0 +1,20 @@
+# GDPR Popup and chatbot blocker extension for Chrome
+
+## Why?
+
+90% of websurfing today is shutting down popups that get in the way of the content you're looking for and is writter by lawers that hate the internet.
+
+## What?
+
+This Chrome extension removes the DOM elements from the websites you use with, based on a blacklisted list of ids and classes.
+
+## How to help?
+
+1. Install it by downloading and adding as a chrome extension
+2. Add more ids and classes to the content.js file and commit back to this repo
+3. Share this in twitter and your social media
+
+## Legal terms
+
+This and everything else is Apache 2 licensed, use however you want to. Any illegal usage of this is on your own conscience and wallet – I take no responsibility for anything you do. If you've read this far you're unusually interested in legal terms and this extension is probably not for you. Get a life.
+
---
+++ content.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function() {
+    
+    const ids = [
+        'hs-eu-cookie-confirmation', //hubspot gdpr popup
+        'hubspot-messages-iframe-container', //hubspot chatbot
+        'sccm-container',  //sanoma websites popup
+        'intercom-container', //intercom popup
+        'gdpr-banner', //gdpr banner at least on intercom.com
+        'js-gdpr-consent-banner', //gdpr banner on stackoverflow
+        'banner__container', //nytimes gdpr crap
+        'cx_bottom_banner', //nbcnews
+        'privacy-consent', //theverge
+        'chatbot-chat' //chatbot.com
+    ];
+
+    const classes = [
+        'alma-data-policy-banner', //alma privacy
+        'as-oil-greeter', // alma popup
+    ]
+
+    //Timeout that waits for async crap to load
+    setTimeout(function(){
+         //Remove ids
+        ids.forEach(function(id) {
+            var element = document.getElementById(id);
+            if (element != null) {
+                console.log('Removing ' + id);
+                element.remove();
+            }
+        });
+
+        //Remove classes
+        classes.forEach(function(cla) {
+            var element = document.getElementsByClassName(cla)[0];
+            if (element != null) {
+                console.log('Removing ' + cla);
+                element.remove();
+            }
+        });
+    },1000);
+   
+});
